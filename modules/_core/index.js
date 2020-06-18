71c120b989cacf67a42648fd2c8b87d7da5c0f5a
Jyles Coad-Ward <jyles@dariox.club>
2020-06-18 20:38:48 +0800

    ported randomHex function

--- modules/_core/index.js
+++ modules/_core/index.js
@@ -1,3 +1,7 @@
 module.exports.tokenManager = function() {
 	return require("./token_manager.js");
 }
+
+module.exports.misc_randHex = function() {
+	return Math.floor(Math.random()*16777215).toString(16);
+}
