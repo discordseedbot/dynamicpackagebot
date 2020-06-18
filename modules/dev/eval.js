373bdce72a8835dccf36fc2f03fef989aeca25cd
Jyles Coad-Ward <jyles@dariox.club>
2020-06-18 18:11:12 +0800

    added regex for eval
    code from https://medium.com/on-discord/how-to-prevent-leaking-your-discord-bots-token-6125118e1222

--- modules/dev/eval.js
+++ modules/dev/eval.js
@@ -2,11 +2,23 @@ const Discord = require("discord.js");
 const { RichEmbed } = require("discord.js");
 const package = require('./../../package.json');
 
+function escapeRegExp(str) {
+	return str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, '\\$1');
+}
+
+function evaluate(input) {
+	let result = eval(input);
+	if (result)
+		result = result.toString().replace(new RegExp(escapeRegExp(token), 'g'), '<token removed>');
+	return result;
+}
+
 module.exports.cmd = function(message, args, client) {
 	if (message.author.id === package.ownerID) {
 		const code = args.join(" ");
-		let evaled = eval(code);
+		let evaled = evaluate(code);
 
 		evaled = require("util").inspect(evaled);
+		message.channel.send("```"+evaled+"```")
 	}
 }
