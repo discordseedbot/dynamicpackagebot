42f09e71989d8f9b143a9de7f05e3b82ce174821
Jyles Coad-Ward <jyles@dariox.club>
2020-06-25 12:18:49 +0800

    Update index.js
    - Added more commands to switch case
    - con.*() -> termcon.*()
    - fixed the command handler
    - starts terminal after seedbot has logged in.

--- modules/_terminal/index.js
+++ modules/_terminal/index.js
@@ -1,13 +1,12 @@
 var inquirer = require("inquirer");
-const {Signale} = require("signale");
-global.Client; global.TokenFunction; global.Libraries;
+const { Signale } = require("signale");
+global.SB_Client; global.SB_TokenFunction; global.SB_Libraries;
 
-global.con = new Signale({
+global.termcon = new Signale({
     disabled: false,
     interactive: false,
     logLevel: 'info',
     scope: 'Terminal',
-    secrets: [],
     stream: process.stdout,
     types: {
         err: {
@@ -27,19 +26,26 @@ global.con = new Signale({
             color: 'green'
         },
         returnValue: {
-            scope: 'Return Value'
+            label: 'Return Value',
+            color: 'yellow'
+        },
+        seeya: {
+            label: "Quitting SeedBot",
+            color: 'yellow',
         }
     }
 });
 
-module.exports = async function(a,b,c) {
-    Client = a;
-    TokenFunction = b;
-    Libraries = c;
-    setTimeout(async function(a,b,c) {
+module.exports = function(a,b,c) {
+    SB_Client = a;
+    SB_TokenFunction = b;
+    SB_Libraries = c;
+    SB_Client.on('ready', () => {
         console.log("Waiting a tad bit before launching the Developer Console.");
-        termHandle(a,b,c);
-    }, 3000)
+        setTimeout(function(a,b,c) {
+            termHandle(a,b,c);
+        }, 2500)
+    })
 }
 
 async function termHandle() {
@@ -57,25 +63,41 @@ async function termHandle() {
     })
 }
 
-async function commandHandler(cmd) {
-    console.log(cmd)
+function commandHandler(cmd) {
 
     // check if the command is valid
 
+    let validCommands = require("./commands.json").commands;
+    var cmdFunc = require("./commandHandle.js");
+
     if (validCommands.indexOf(cmd[0]) > -1){
-        console.log(cmd)
         //command is valid
-        var cmdFunc = require("./commandHandle.js")
-        switch (cmd) {
+        switch (cmd[0]) {
             case "list":
-            case "print":
             case "get":
+            case "print":
                 cmdFunc.print(cmd);
+                break;
+            case "set":
+                cmdFunc.set(cmd);
+                break;
+            case "eval":
+            case "run":
+                cmdFunc.eval(cmd);
+                break;
+            case "exit":
+            case "quit":
+                termcon.seeya()
+                process.exit();
+                break;
+            default:
+                console.error(new Error());
+                process.exit(0);
                 break;
         }
     } else {
         //command is invalid
-        con.invalidCommand()
+        termcon.invalidCommand()
         return false;
     }
 
