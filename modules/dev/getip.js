c08df7136549376e5947de280f8d8f1617950e3d
Jyles Coad-Ward <jyles@dariox.club>
2020-07-04 15:20:45 +0800

    re-did the entire backend thig

--- modules/dev/getip.js
+++ modules/dev/getip.js
@@ -1,26 +1,31 @@
 const Discord = require("discord.js");
 const { RichEmbed } = require("discord.js");
 const package = require('./../../package.json');
+const http = require("http")
 
 module.exports.cmd = function(message, args) {
-	const extIP = require('external-ip');
-	let useragent = "+SeedBot-" && package.version && " http://seedbot.jyles.club";
-	let getIP = extIP({ eplace: true, services: ['http://dxcdn.net/api/public-ip.php'], timeout: 600, getIP: 'parallel', userAgent: useragent});
-	var type = args.slice(0).join(' ');
-	if (message.author.id === package.ownerID){
-		getIP((err, ip) => {
-			if (err) {
-				throw err;
-			}
-				let evalEmbed = new Discord.RichEmbed()
+
+	http.get("http://api.ipify.org", (res) => {
+        let rawData = '';
+        res.on('data', (chunk) => { rawData += chunk; });
+        res.on('end', () => {
+            try {
+				var type = args.slice(0).join(' ');
+				let evalEmbed = new Discord.MessageEmbed()
 					.setColor('#0099ff')
 					.setTitle('Get Global IP Address')
 					.setAuthor('s~getip')
 					.setTimestamp()
-					.setDescription('Global IP Address:\n```\n' + ip + "\n```");
+					.setDescription('Global IP Address:\n```\n' + rawData + "\n```");
 				message.channel.send(evalEmbed);
-			});
-	} else {
-		message.reply('You do not have permission to use this developer command\nSorry!');
-	}
+            } catch (e) {
+				SB_Libraries.forEach(async (m) => {
+					if (m.name === "developer_alerts") {
+						let tmpRequire = require(`./../../${m.location}/${m.main}`).developerNotif(tmpNotifContent);
+					}
+				})
+            }
+        });
+    })
+
 }
