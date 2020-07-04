5b8a8c7a454c316e1487f227bf965173c50da031
Jyles Coad-Ward <jyles@dariox.club>
2020-07-04 15:20:28 +0800

    bug fixes

--- modules/dev/rpc.js
+++ modules/dev/rpc.js
@@ -1,24 +1,26 @@
 const Discord = require("discord.js");
 const { RichEmbed } = require("discord.js");
-const SB_Client = new Discord.Client();
-const prefix = require("./../../prefix.json").dev;
-const package = require('./../../package.json');
+const prefix = SB_CoreLibrary.prefix().dev;
 
 module.exports.cmd = function(message,args) {
 	var game = args.slice(0).join(" ");
-	if (message.author.id === package.ownerID) {
 		if (game.length > 1){
-			if (game === 'refresh') {
-				SB_Client.user.setActivity(`you - seedbot.xyz`, { type: 'WATCHING'});
-				message.channel.send("***Rich Presence has been Refreshed***")
-			} else {
-				SB_Client.user.setActivity(`${game} - seedbot.xyz`, { type: 'WATCHING'});
-				message.channel.send('***Rich Presence has been updated to:*** \n' + "`" + game + "`");
+			switch (game) {
+				case "refresh":
+				case "reset":
+					SB_Client.user.setActivity(`you - seedbot.xyz`, { type: 'WATCHING'});
+					message.channel.send("***Rich Presence has been Refreshed***")
+					break;
+				case "clear":
+					SB_Client.user.setActivity(null);
+					message.channel.send("Rich Presence has been set to `null` (nothing)")
+					break;
+				default:
+					SB_Client.user.setActivity(`${game} - seedbot.xyz`, { type: 'WATCHING'});
+					message.channel.send('***Rich Presence has been updated to:*** \n' + "`" + game + "`");
+					break;
 			}
 		} else {
-			message.reply("No Arguments specified.")
+			message.reply("No Arguments Given, Check Documentation")
 		}
-	}else{
-		message.reply('You do not have permissions to use this developer command.');
-	}
 }
