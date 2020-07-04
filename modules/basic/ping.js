5db51d692c906c9038b0e67fb41e88b3c1cdcaf6
Jyles Coad-Ward <jyles@dariox.club>
2020-07-04 13:42:32 +0800

    global variables now in use

--- modules/basic/ping.js
+++ modules/basic/ping.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const { RichEmbed } = require("discord.js");
 const response = require("./response.json");
 
-module.exports.cmd = async function(message,lib) {
+module.exports.cmd = async function(message) {
 	const m = await message.channel.send("Ping?");
 	m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. \nAPI Latency is ${Math.round(SB_Client.ping)}ms`);
 }
--- modules/basic/roadmap.js
+++ modules/basic/roadmap.js
@@ -2,9 +2,9 @@ const Discord = require("discord.js");
 const { RichEmbed } = require("discord.js");
 const response = require("./response.json");
 
-module.exports.cmd = function(message,lib) {
+module.exports.cmd = function(message) {
 	let evalEmbed = new Discord.RichEmbed()
-		.setColor(lib.misc_randHex())
+		.setColor(SB_CoreLibrary.misc_randHex())
 		.setTitle(response.roadmap.title)
 		.setDescription(response.roadmap.desc)
 		.setTimestamp()
