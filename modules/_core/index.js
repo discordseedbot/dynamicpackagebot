d0d92a8ddd0666ce2058503c846e8f608d4728dc
Jyles Coad-Ward <jyles@dariox.club>
2020-07-06 00:37:01 +0800

    added guildCount, channelCount, userCount functions

--- modules/_core/index.js
+++ modules/_core/index.js
@@ -17,3 +17,43 @@ module.exports.prefrences = function() {
 module.exports.prefix = function() {
 	return require("./../../prefrences.json").prefix;
 }
+
+module.exports.channelCount = function() {
+	let retval={"channels":0,"guilds":0,"members":0};
+	SB_Client.guilds.cache.forEach(m => {
+	  //retval.members+=m.memberCount
+	  //retval.guilds+=1
+	  retval.channels+=m.channels.cache.size
+	})
+	return retval.channels;
+}
+module.exports.guildCount = function() {
+	let retval={"channels":0,"guilds":0,"members":0};
+	SB_Client.guilds.cache.forEach(m => {
+	  //retval.members+=m.memberCount
+	  retval.guilds+=1
+	  //retval.channels+=m.channels.cache.size
+	})
+	return retval.guilds;
+}
+module.exports.userCount = function() {
+  let retval={"channels":0,"guilds":0,"members":0};
+  SB_Client.guilds.cache.forEach(m => {
+    retval.members+=m.memberCount
+    //retval.guilds+=1
+    //retval.channels+=m.channels.cache.size
+  })
+  return retval.members;
+}
+
+module.exports.toHHMMSS = function(beans) {
+  var sec_num = parseInt(beans, 10); // don't forget the second param
+  var hours   = Math.floor(sec_num / 3600);
+  var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+  var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+  if (hours   < 10) {hours   = "0"+hours;}
+  if (minutes < 10) {minutes = "0"+minutes;}
+  if (seconds < 10) {seconds = "0"+seconds;}
+  return hours+':'+minutes+':'+seconds;
+}
