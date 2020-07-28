943922757f0a20f6a8576bcde2744afab178b40f
Jyles Coad-Ward <jyles@dariox.club>
2020-07-28 16:35:59 +0800

    optimisation

--- index.js
+++ index.js
@@ -6,14 +6,8 @@
 
 //			Check if node mdoules are installed
 const fs = require("fs");
-
-try {
-	if (!fs.existsSync("node_modules/")) {
-		console.log("Node Modules were not installed, try `npm i`");
-		process.exit(1);
-	}
-} catch (e) {
-	console.error(e);
+if (!fs.existsSync("node_modules/")) {
+	console.log("Node Modules were not installed, try `npm i`");
 	process.exit(1);
 }
 
@@ -24,10 +18,17 @@ try {
 	global.SB_Prefrences = require("./prefrences.json");
 	global.signale = require("signale");
 } catch (e) {
-	console.log("Error Occoured when declaring Global Variables.");
+	signale.error("An error Occoured when declaring [GlobalVariables]");
 	console.error(e);
 	process.exit(1);
 }
+
+//			Check if SeedBot was launched in DebugMode
+global.SB_Debug = false;
+if(process.argv.indexOf("--debug") > -1){
+	global.SB_Debug = true;
+}
+
 console.clear();
 function getDirectories(path) {
   return fs.readdirSync(path).filter(function (file) {
@@ -37,7 +38,6 @@ function getDirectories(path) {
 
 var moduleArray = getDirectories("modules/");
 var viableModules = [];
-
 
 //			Check each of the directories in "modules/" if they have a "manifest.json" file.
 moduleArray.forEach(async (m) => {
@@ -49,13 +49,11 @@ moduleArray.forEach(async (m) => {
 	}
 })
 
-
 //			If there are no valid modules quit process.
 if (viableModules.length < 1) {
 	signale.log("[modman] No valid modules found.");
 	process.exit(0);
 }
-
 
 //			Check if manifest for the module is valid
 viableModules.forEach(async (m) => {
@@ -76,37 +74,32 @@ viableModules.forEach(async (m) => {
 		}
 		console.error(e)
 	}
-	delete(jsontemp);
-
 })
-
 
 //			Check if any of the modules are libraries and if they are, remove them from the viableModules array.
 var botModulesToLoad = [];var genericModulesToLoad = [];var libraries = []; let tmparr;
+
+//			Run this function for every module found in the varaible viableModules
 viableModules.forEach(async (m) => {
 	try {
 		let jsontemp = require(`./${m}/manifest.json`);
 		let filepush = `${m}/${jsontemp.main}`;
-		if (m.indexOf('example') !== -1 || m.indexOf('test') !== -1) {
-			signale.warn("Example Module Detected, Not Loading.");
-			jsontemp.type = "example";
-			return;
+		if (!SB_Debug) {
+			if (m.indexOf('example') !== -1 || m.indexOf('test') !== -1) {
+				genericModuleConsole.notLoad("Example Module was disabled [Not in Debug Mode]");
+				jsontemp.type = "example";
+				return;
+			}
 		}
 		switch (jsontemp.type) {
 			case "botmod":
-				tmparr = JSON.parse(`{"name": "${jsontemp.name}","main": "${jsontemp.main}","location":"${m}"}`);
-				botModulesToLoad.push(tmparr);
-				delete(tmparr);
+				botModulesToLoad.push(		JSON.parse(`{"name": "${jsontemp.name}","main": "${jsontemp.main}","location":"${m}"}`));
 				break;
 			case "generic":
-				tmparr = JSON.parse(`{"name": "${jsontemp.name}","main": "${jsontemp.main}","location":"${m}"}`);
-				genericModulesToLoad.push(tmparr);
-				delete(tmparr);
+				genericModulesToLoad.push(	JSON.parse(`{"name": "${jsontemp.name}","main": "${jsontemp.main}","location":"${m}"}`));
 				break;
 			case "library":
-				tmparr = JSON.parse(`{"name": "${jsontemp.name}","main": "${jsontemp.main}","location":"${m}"}`);
-				libraries.push(tmparr);
-				delete(tmparr);
+				libraries.push(				JSON.parse(`{"name": "${jsontemp.name}","main": "${jsontemp.main}","location":"${m}"}`));
 				break;
 			default:
 				signale.warn(`[modman] Unknown Module type at "${m}/manifest.json"`);
@@ -119,34 +112,41 @@ viableModules.forEach(async (m) => {
 })
 
 
-//			Set Global Functions and Varaibles, very important!
-try {
-	global.SB_BotModules = botModulesToLoad;
-	global.SB_GenericModules = genericModulesToLoad;
-	libraries.forEach(async (m) => {
-		if (m.name === "core") {
-			// Setup the token varaible for the modules (if they are needed, in most cases they are.)
-			var corelib = require(`./${m.location}/${m.main}`)
-			global.SB_TokenFunction = corelib.tokenManager();
-			global.SB_Token = corelib.tokenManager();
-			global.SB_Libraries = libraries;
-			global.SB_CoreLibrary = corelib;
-			corelib.consoleInit()
-		}
-	})
-} catch (e) {
-	console.error(`[Global Functions]\n${e}`);
+
+//			Set Global Variables.
+global.SB_BotModules = botModulesToLoad;
+global.SB_GenericModules = genericModulesToLoad;
+var coreFound = false;
+libraries.forEach(async (m) => {
+	if (m.name === "core") {
+		// Setup the token varaible for the modules (if they are needed, in most cases they are.)
+		var corelib = require(`./${m.location}/${m.main}`)
+		global.SB_TokenFunction = corelib.tokenManager();
+		global.SB_Token = corelib.tokenManager();
+		global.SB_Libraries = libraries;
+		global.SB_CoreLibrary = corelib;
+		SB_CoreLibrary.consoleInit()
+		coreFound = true;
+	}
+})
+if (!coreFound) {
+	signale.error("Core Library was not found. Process Halted.");
+	delete(coreFound);
 	process.exit(1);
 }
 
 
-//			Discord Setup Stuff
+//			Discord.JS Login with Error Catching.
 const Discord = require('discord.js');
 global.SB_Client = new Discord.Client();
 SB_Client.login(SB_Token.discord()).catch(async function (e) {
 	switch(e.code) {
 		case "SELF_SIGNED_CERT_IN_CHAIN":
 			signale.error("Self-Signed certificate found in chain.");
+			process.exit(1);
+			break;
+		case "TOKEN_INVALID":
+			signale.error("Discord Token is Invalid.")
 			process.exit(1);
 			break;
 		default:
@@ -156,14 +156,22 @@ SB_Client.login(SB_Token.discord()).catch(async function (e) {
 	}
 });
 
-
-//			Modules are executed, if one crashes it does not take the entire program with it, because of asynchronous functions
-//				or some shit like that, i didn't go to uni.
-botModulesToLoad.forEach(async (m) => {
-	botModuleConsole.attemptLoad(m.name);
-	var runDiscordModule = require(`./${m.location}/${m.main}`)();
+//			From this point all errors should be from the modules.
+SB_Client.on('ready', function(){
+	if (!SB_Debug) {
+		console.clear()
+		signale.complete("Discord Bot has Logged In");
+	} else {
+		console.log(`- - - - - Discord Bot Logged In - - - - -`)
+	}
 });
-genericModulesToLoad.forEach(async (m) => {
-	genericModuleConsole.attemptLoad(m.name)
-    var runDiscordModule = require(`./${m.location}/${m.main}`)();
-});
+setTimeout(async function() {
+	botModulesToLoad.forEach(async (m) => {
+		botModuleConsole.attemptLoad(`${m.name}@${require("./"+m.location+"/manifest.json").version}`)
+		require(`./${m.location}/${m.main}`)();
+	});
+	genericModulesToLoad.forEach(async (m) => {
+		genericModuleConsole.attemptLoad(`${m.name}@${require("./"+m.location+"/manifest.json").version}`);
+	    require(`./${m.location}/${m.main}`)();
+	});
+}, 300)
