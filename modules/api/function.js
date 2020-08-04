be3b2e3096783ff8fad3b2d77db0dc099ea74126
Jyles Coad-Ward <jyles@dariox.club>
2020-08-04 13:05:37 +0800

    more debug logging

--- modules/api/function.js
+++ modules/api/function.js
@@ -21,12 +21,15 @@ switch (SB_Prefrences.api.network.port) {
 	case "auto":
 	case "default":
 	case "autodetect":
+		if (SB_Debug) {apicon.debug("Port is set to [AutoDetect]")}
 		base = `${SB_Prefrences.api.network.protocol.type}://${SB_Prefrences.api.network.address}/`;
 		break;
 	default:
 		if (/^\d+$/.test(SB_Prefrences.api.network.port)) {
+			if (SB_Debug) {apicon.debug("Port is set to [${SB_Prefrences.api.network.port}]")}
 			base = `${SB_Prefrences.api.network.protocol.type}://${SB_Prefrences.api.network.address}:${SB_Prefrences.api.network.port}/`;
 		} else {
+			if (SB_Debug) {apicon.debug("Port is set to [AutoDetect]")}
 			base = `${SB_Prefrences.api.network.protocol.type}://${SB_Prefrences.api.network.address}/`;
 			termcon.err("Invalid Port given for API in `prefrences.json`, Must only contain numbers.");
 		}
@@ -36,7 +39,7 @@ if (SB_Debug) {apicon.debug(`Base Request Set to [${base}]`)};
 
 console.log(base)
 
-module.exports.checkConnection = function() {
+module.exports.checkConnection = async function() {
 	pfx.get(`${base}?req=checkConnection`, (res) => {
 		res.on('data', (d) => {
 			if (d === 'true') {
@@ -47,7 +50,7 @@ module.exports.checkConnection = function() {
 		})
 	})
 }
-module.exports.sendRequest = function (r,d) {
+module.exports.sendRequest = async function (r,d) {
 	if (pfx !== undefined) {
 		pfx.get(`${base}?req=${r}&data=${d}&token=${SB_Token.apiToken()}`,  (res) => {
 			if (SB_Debug) {apicon.debug(`Request [${r}] with the data of [${d}]`)};
@@ -60,6 +63,7 @@ module.exports.getRequest = async function (r) {
 	if (pfx !== undefined) {
 		let url = `${base}?req=${r}`;
 		pfx.get(url, async (res) => {
+			if (SB_Debug) {apicon.debug(`Request [${r}] was sent to the API server.`)};
 			res.on('data', (d) => {
 				return `${d}`;
 			})
@@ -71,19 +75,15 @@ module.exports.getRequest = async function (r) {
 module.exports.goOnline = async function() {
 	if (pfx !== undefined) {
 		if (SB_Debug) apicon.debug(`"goOnline" packet sent.`);
-		pfx.get(`${base}?token=${SB_Token.apiToken()}&req=isOnline&data=online`);
+		require("./function.js").sendRequest("isOnline","online");
 	}
 }
 module.exports.goOffline = async function() {
 	if (token.apiToken() !== "seedbot-api-token"){
 		if (pfx !== undefined) {
-			let url = `${base}?req=isOnline&data=offline&token=${SB_Token.apiToken()}`
-			pfx.get(url, async (res) => {
-				res.on('data', (d) => {
-					if (SB_Debug) apicon.debug(`"goOffline" packet sent.`);
-					setTimeout(process.exit(),300);
-				})
-			})
+			require("./function.js").sendRequest("isOnline","online");
+			if (SB_Debug) apicon.debug(`"goOffline" packet sent.`);
+			setTimeout(process.exit(),300);
 		}
 	} else {
 		process.exit();
