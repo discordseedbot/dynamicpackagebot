fbf6f956bec8de762a4cd45fac19d1e7551cdae1
Jyles Coad-Ward <jyles@dariox.club>
2020-08-06 20:27:41 +0800

    ported music module

---
+++ modules/music/index.js
@@ -0,0 +1,26 @@
+module.exports = async function() {
+	const music = require('./13-9-10.js');
+	var errorCH;
+	if (SB_Prefrences.developer_notif.enable) {
+		errorCH = SB_Prefrences.developer_notif.userspaceError.error
+	}  else {
+		errorCH = undefined;
+	}
+	music.start(SB_Client, {
+	  youtubeKey: SB_Token.youtube(),
+	  cooldown:{
+	    disabled:false,
+	    timer:10
+	  },
+	  botPrefix: SB_CoreLibrary.prefix().music,
+	  anyoneCanSkip: true,
+	  anyoneCanAdjust: true,
+	  inlineEmbeds: false,
+	  logging: false,
+	  errorChannel: errorCH
+	});
+
+	SB_Client.on('ready', () => {
+		botModuleConsole.loaded("Music");
+	})
+}
