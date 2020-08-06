d67f8e05a187b238a0af04a789cdd9083e443eae
Jyles Coad-Ward <jyles@dariox.club>
2020-08-06 20:26:56 +0800

    checks if enabled in prefrences

--- modules/developer_alerts/index.js
+++ modules/developer_alerts/index.js
@@ -11,6 +11,7 @@ function addMsgInfo(message) {
 
 // Developer Error
 module.exports.developerError = async function(message,error) {
+	if (!SB_Prefrences.developer_notif.enable) return;
     if (message.author.bot) return;
     addMsgInfo(message)
     let tmp = content
@@ -22,6 +23,7 @@ module.exports.developerError = async function(message,error) {
 }
 // Userspace Error
 module.exports.userspaceError  = async function(message,error) {
+	if (!SB_Prefrences.developer_notif.enable) return;
     if (message.author.bot) return;
     addMsgInfo(message)
     let tmp = content.setTitle("Developer Error")
@@ -32,16 +34,19 @@ module.exports.userspaceError  = async function(message,error) {
 
 // Custom Developer Notifications
 module.exports.developerNotif = async function (content) {
+	if (!SB_Prefrences.developer_notif.enable) return;
     SB_Client.channels.cache.get(channelJSON.developer.notifications).send(content)
 }
 
 // Custom Userspace Notifications
 module.exports.userspaceNotif = async function (content) {
+	if (!SB_Prefrences.developer_notif.enable) return;
     SB_Client.channels.cache.get(channelJSON.userspace.notifications).send(content)
 }
 
 // Developer Alert (e.g developer used command, bot joined server, bot kicked from server, etc...)
 module.exports.developerAlert = async function (message,error) {
+	if (!SB_Prefrences.developer_notif.enable) return;
     if (message.author.bot) return;
     addMsgInfo(message)
     let tmp = content.setTitle("Developer Alert")
@@ -52,6 +57,7 @@ module.exports.developerAlert = async function (message,error) {
 
 // Developer Unauthorized Access (user that is not in the config attempted to load a command.)
 module.exports.developerUnauth = async function (message,error) {
+	if (!SB_Prefrences.developer_notif.enable) return;
         // Tell the user they're a dumb cunt.
     SB_Client.on('message', async message => {
         let invalidAuthor = new Discord.MessageEmbed()
