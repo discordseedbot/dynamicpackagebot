8322a5135be628e88baf8c8a085bc5f344062db2
Jyles Coad-Ward <jyles@dariox.club>
2020-09-17 12:52:58 +0800

    ported api module

--- modules/api/function.js
+++ modules/api/function.js
@@ -1,11 +1,11 @@
 function validProtocolType() {
-	switch (SB_Prefrences.api.network.protocol.type) {
+	switch (SB.prefrences.api.network.protocol.type) {
 		case "https":
-			if (SB_Debug) apicon.debug(`Protocol Type Detected as "${SB_Prefrences.api.network.protocol.type}"`);
+			if (SB.parameters.debugMode) SB.con.api.debug(`Protocol Type Detected as "${SB_Prefrences.api.network.protocol.type}"`);
 			return require("https");
 			break;
 		case "http":
-			if (SB_Debug) apicon.debug(`Protocol Type Detected as "${SB_Prefrences.api.network.protocol.type}"`);
+			if (SB.parameters.debugMode) SB.con.api.debug(`Protocol Type Detected as "${SB_Prefrences.api.network.protocol.type}"`);
 			return require("http");
 			break;
 		default:
@@ -17,25 +17,25 @@ const pfx = validProtocolType();
 
 var base;
 //if (SB_Debug) apicon.debug(``);
-switch (SB_Prefrences.api.network.port) {
+switch (SB.prefrences.api.network.port) {
 	case "auto":
 	case "default":
 	case "autodetect":
-		if (SB_Debug) {apicon.debug("Port is set to [AutoDetect]")}
-		base = `${SB_Prefrences.api.network.protocol.type}://${SB_Prefrences.api.network.address}/`;
+		if (SB.parameters.debugMode) {SB.con.api.debug("Port is set to [AutoDetect]")}
+		base = `${SB.prefrences.api.network.protocol.type}://${SB.prefrences.api.network.address}/`;
 		break;
 	default:
-		if (/^\d+$/.test(SB_Prefrences.api.network.port)) {
-			if (SB_Debug) {apicon.debug(`Port is set to [${SB_Prefrences.api.network.port}]`)}
-			base = `${SB_Prefrences.api.network.protocol.type}://${SB_Prefrences.api.network.address}:${SB_Prefrences.api.network.port}/`;
+		if (/^\d+$/.test(SB.prefrences.api.network.port)) {
+			if (SB.parameters.debugMode) {SB.con.api.debug(`Port is set to [${SB.prefrences.api.network.port}]`)}
+			base = `${SB.prefrences.api.network.protocol.type}://${SB.prefrences.api.network.address}:${SB.prefrences.api.network.port}/`;
 		} else {
 			if (SB_Debug) {apicon.debug("Port is set to [AutoDetect]")}
-			base = `${SB_Prefrences.api.network.protocol.type}://${SB_Prefrences.api.network.address}/`;
-			termcon.err("Invalid Port given for API in `prefrences.json`, Must only contain numbers.");
+			base = `${SB.prefrences.api.network.protocol.type}://${SB_Prefrences.api.network.address}/`;
+			SB.con.api.err("Invalid Port given for API in `prefrences.json`, Must only contain numbers.");
 		}
 		break;
 }
-if (SB_Debug) {apicon.debug(`Base Request Set to [${base}]`)};
+if (SB.parameters.debugMode) {SB.con.api.debug(`Base Request Set to [${base}]`)};
 
 module.exports.checkConnection = async function() {
 	pfx.get(`${base}?req=connectionTest`, (res) => {
@@ -45,17 +45,17 @@ module.exports.checkConnection = async function() {
 		})
 		res.on('end',()=>{
 			if (data === 'true') {
-					apicon.info("Connection Established to API Server.");
+					SB.con.api.info("Connection Established to API Server.");
 			} else {
-					apicon.err("Connection failed to API Server, some commands may not work. Disable the API in modules/config.json");
+					SB.con.api.err("Connection failed to API Server, some commands may not work. Disable the API in modules/config.json");
 			}
 		})
 	})
 }
 module.exports.sendRequest = async function (r,d) {
 	if (pfx !== undefined) {
-		pfx.get(`${base}?req=${r}&data=${d}&token=${SB_Token.apiToken()}`,  (res) => {
-			if (SB_Debug) {apicon.debug(`Request [${r}] with the data of [${d}]`)};
+		pfx.get(`${base}?req=${r}&data=${d}&token=${SB.token.api}`,  (res) => {
+			if (SB.parameters.debugMode) {SB.con.api.debug(`Request [${r}] with the data of [${d}]`)};
 			return "poo poo";
 		})
 	}
@@ -65,7 +65,7 @@ module.exports.getRequest = async function (r) {
 	if (pfx !== undefined) {
 		let url = `${base}?req=${r}`;
 		pfx.get(url, async (res) => {
-			if (SB_Debug) {apicon.debug(`Request [${r}] was sent to the API server.`)};
+			if (SB.parameters.debugMode) {SB.con.api.debug(`Request [${r}] was sent to the API server.`)};
 			res.on('data', (d) => {
 				return `${d}`;
 			})
@@ -76,15 +76,15 @@ module.exports.getRequest = async function (r) {
 
 module.exports.goOnline = async function() {
 	if (pfx !== undefined) {
-		if (SB_Debug) apicon.debug(`"goOnline" packet sent.`);
+		if (SB.parameters.debugMode) SB.con.api.debug(`"goOnline" packet sent.`);
 		require("./function.js").sendRequest("isOnline","online");
 	}
 }
 module.exports.goOffline = async function() {
-	if (token.apiToken() !== "seedbot-api-token"){
+	if (SB.token.api !== "seedbot-api-token"){
 		if (pfx !== undefined) {
 			require("./function.js").sendRequest("isOnline","online");
-			if (SB_Debug) apicon.debug(`"goOffline" packet sent.`);
+			if (SB.parameters.debugMode) SB.con.api.debug(`"goOffline" packet sent.`);
 			setTimeout(process.exit(),300);
 		}
 	} else {
