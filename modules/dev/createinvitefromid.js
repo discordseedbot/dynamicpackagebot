7d474e139a8d5655d941156ef4d30b395abecf4c
Jyles Coad-Ward <jyles@dariox.club>
2020-12-17 15:17:22 +0800

    fixed createinvite command

--- modules/dev/createinvitefromid.js
+++ modules/dev/createinvitefromid.js
@@ -1,11 +1,16 @@
 const package = require('./../../package.json');
 
 module.exports.cmd = function(message,args) {
-        let guildid = args.slice(0).join(' ');
-        let guild = SB.client.guilds.cache.get(guildid);
-        if (!guild) return message.reply("The bot isn't in the guild with this ID.");
+	let guildid = args.slice(0).join(' ');
+	let guild = SB.client.guilds.cache.get(guildid);
+	if (!guild) return message.reply("The bot isn't in the guild with this ID.");
 
-        guild.fetchInvites()
-            .then(invites => message.channel.send('Found Invites:\nhttps://discord.gg/' + invites.map(invite => invite.code).join('\n')))
-            .catch(console.error);
+	var channelToCreateInviteIn = {};
+	SB.client.guilds.cache.get(guildid).channels.cache.array().forEach((channel)=>{
+		if (channel.type.toLowerCase() == 'text') {
+			channelToCreateInviteIn = channel;
+		}
+	})
+	channelToCreateInviteIn.createInvite()
+	.then(invite => message.reply(`Created an invite with a code of ${invite.code}\r\nhttps://discord.gg/${invite.code}`))
 }
